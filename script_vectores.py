94b247f203b54a47c58e7677d21c847201b95070
Ignacio Patiño <ignaciocorreagabbi@gmail.com>
2023-07-26 12:51:53 -0300

    borre cambios en el script

--- script_vectores.py
+++ script_vectores.py
@@ -11,17 +11,12 @@ def producto_escalar(v1, v2):
 
 def son_ortogonales(v1, v2):
     
-    producto_punto = v1[0] * v2[0]
-    posicion = 1
-
-    for n in range(2):
-        producto_punto = producto_punto + (v1[posicion] * v2[posicion])
-        posicion += 1
+    '''
+    Verificar si dos vectores dados son ortogonales. 
     
-    if producto_punto == 0:
-        return f"El producto punto es {producto_punto}, por lo que ambos vectores SI son perpendiculares"
-    else:
-        return f"El producto punto es {producto_punto}, por lo que ambos vectores NO son perpendiculares"
+    '''
+        
+    return
 
 def son_paralelos(v1, v2):
 
